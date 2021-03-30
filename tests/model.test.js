918cbd8f126f38a4dd9db81b903ae6ffaaacbc8e
hyfydistro <hyfydistro@outlook.com>
2021-03-31 01:18:25 +0700

    test(scripts): add test for 'addContact' method

---
+++ tests/model.test.js
@@ -0,0 +1,115 @@
+import Model from "../src/scripts/model.js"
+import getUniqueId from "../src/scripts/libs/idGenerator"
+
+const sample = {
+  id: getUniqueId(),
+  name: "john",
+  email: "john@example.com",
+  phone: "",
+  favorite: false
+}
+
+// TODO
+// write test cases for following "todo"
+
+const model = new Model();
+
+describe("when data is added", () => {
+  test("if array is not empty", () => {
+    model.addContact("john");
+    const modelLength = model.contactlist.length;
+
+    expect(modelLength).toBeGreaterThan(0);
+  })
+  test("if value exist", () => {
+    model.addContact("john");
+
+    expect(model.contactlist[0].name).toBe("john")
+  })
+
+
+  describe("should have key and value", () => {
+    test("if \"name\" has correct key and value", () => {
+      model.addContact("john");
+
+      expect(model.contactlist[0]).toEqual(expect.objectContaining({name: "john"}));
+      })
+    test("if \"email\" has correct key and value", () => {
+      model.addContact("john", "john@example.com");
+
+      expect(model.contactlist).toEqual(
+        expect.arrayContaining(
+            [
+              expect.objectContaining({email: "john@example.com"})
+            ]
+          )
+        );
+      })
+      // ! WIP
+      test.todo("if \"phone\" has correct key and value")
+      test.todo("if \"favorite\" has correct key and value")
+  })
+
+
+  describe("should not have any values that are undefined", () => {
+    test("if \"id\" is not undefined", () => {
+      model.addContact("john");
+      const modelLength = model.contactlist.length;
+      const result = model.contactlist[modelLength-1].id;
+
+      expect(result).not.toBeUndefined();
+    })
+    test("if \"name\" is not undefined", () =>{
+      model.addContact("john");
+      const modelLength = model.contactlist.length;
+      const result = model.contactlist[modelLength-1].name;
+
+      expect(result).not.toBeUndefined();
+    })
+    test("if \"email\" is not undefined", () => {
+      model.addContact("john", "john@example.com");
+      const modelLength = model.contactlist.length;
+      const result = model.contactlist[modelLength-1].email;
+
+      expect(result).not.toBeUndefined();
+    })
+    test("if \"phone\" is undefined", () => {
+      model.addContact("john", "john@example.com", "555-555-1234");
+      const modelLength = model.contactlist.length;
+      const result = model.contactlist[modelLength-1].phone;
+
+      expect(result).not.toBeUndefined();
+    })
+  })
+
+  test("if \"favorite\" is boolean false", () => {
+    model.addContact("john");
+    const modelLength = model.contactlist.length;
+    const result = model.contactlist[modelLength-1].favorite;
+
+    expect(result).toBeFalsy();
+  })
+
+
+  // ! WIP (current)
+  describe("should have valid values", () => {
+    test("if \"email\" has valid value", () => {
+      model.contactlist = [];
+      model.addContact("john", "john@example.com");
+      const regex =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      const result = model.contactlist[0].email;
+
+      expect(result).toMatch(regex);
+    })
+
+    // ! WIP
+    test.todo("if email have correct format");
+    test.todo("if phone number have correct format");
+  })
+
+})
+
+
+// ! WIP
+// use `describe`
+test.todo("if methods are called")
