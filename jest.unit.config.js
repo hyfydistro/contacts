f8202aaaf67fcb3f45f95228cbcf37a0b2bf3359
hyfydistro <hyfydistro@outlook.com>
2021-04-30 14:10:16 +0700

    test(jest): fix invalid regular expression

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -2,5 +2,5 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.unit.js'],
   testEnvironment: "jsdom",
   testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-  testPathIgnorePatterns: ["/node_modules/", "tests/e2e/**/*.js", "tests/demo.js"]
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/tests/e2e/", "tests/demo.js"]
 };
