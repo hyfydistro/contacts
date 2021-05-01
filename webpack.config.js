3a05e6517e80b0ce0ec2ee801838790dac632418
hyfydistro <hyfydistro@outlook.com>
2021-05-01 11:32:12 +0700

    build: add "style-loader", "compression-webpack-plugin", ""sxpress", "body-parser"
    configure CompressionPlugin in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// compression plugin
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
 
 // Coupled with setting NODE_ENV via npm "scripts"
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
@@ -20,6 +23,7 @@ module.exports = {
     assets: "./src/assets.js"
   },
   output: {
+    // filename: "[name].bundler.js",
     filename: "[name].bundler.js",
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]"
@@ -52,12 +56,13 @@ module.exports = {
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: ""
-            }
-          },
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     publicPath: ""
+          //   }
+          // },
+          "style-loader",
           "css-loader",
           "postcss-loader",
           "sass-loader",
@@ -69,11 +74,57 @@ module.exports = {
   // # Plugins
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    // new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       title: "Contacts Web App",
       template: path.resolve(__dirname, "src", "index.html"),
       inject: "body",
+    }),
+
+    // # ONLY for PRODUCTION
+    // Otherwise, comment
+
+    // with gzip
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+
+      // include: /src/
+      // include: /dist/
+      algorithm: "gzip",
+
+      // test: /\.(js|css|html)$/,
+      test: /\.(js)$/,
+
+      // level 9 is the highest fo gzip, other algorithm may vary (see doc)
+      compressionOptions: {level: 7},
+      // compressionOptions: {level: 9}
+
+      // Only assets bigger than this size are processed (in Bytes)
+      // threshold: 8192
+
+      // Only assets that compress better than this ratio are processed (minRatio = Compressed Size / Original Size)
+      // - you can use `1` value to process assets that are smaller than the original.
+      // - Use a value of `Infinity` to process all assets even if they are larger than the original size or their original size is 0 bytes (useful when you are pre-zipping all assets for AWS)
+      // Use a value of `Number.MAX_SAFE_INTEGER` to process all assets even if they are larger than the original size, excluding assets with their original size is `0` bytes
+      // minRaiot: 0.8 // Default
+
+      // deleteOriginalAssets: true,
+    }),
+
+    // with zilb (Brotl)
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      // test: /\.(js|css|html|svg)$/,
+      test: /\.(js)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false
     })
   ],
 
