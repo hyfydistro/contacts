f8e2d4b92333cf5dc8de5bf9f4d478dc7f3a6fe6
hyfydistro <hyfydistro@outlook.com>
2021-05-07 22:44:26 +0700

    feat: add service worker

---
+++ src/sw.js
@@ -0,0 +1,59 @@
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+console.log("⚙ Hello from Service Worker");
+
+registerRoute(
+  // ({url}) => url.origin === "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css",
+  new RegExp("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
+  new NetworkFirst({
+    cacheName: "font-awesome-stylesheet",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
+
+registerRoute(
+  new RegExp("https://fonts.googleapis.com"),
+  new StaleWhileRevalidate({
+    cacheName: "google-fonts-stylesheets",
+  })
+);
+
+registerRoute(
+  ({url}) => url.origin === "https://fonts.gstatic.com",
+  new CacheFirst({
+    cacheName: "google-fonts-webfonts",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ]
+  })
+);
+
+registerRoute(
+  ({request}) => request.destination === 'script' ||
+                  request.destination === 'style' ||
+                  request.destination === 'document',
+  new CacheFirst({
+    cacheName: 'static-resources',
+  })
+);
+
+// workbox.skipWaiting();
+
+// precacheAndRoute(self.__WB_MANIFEST || []);
